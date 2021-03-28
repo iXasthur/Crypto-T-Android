664459d15abd4006986876c39e0733d621591898
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2021-03-28 13:54:56 +0300

    Refactored paths

--- app/src/main/java/com/xcnk/cryptot/screens/auth/AuthActivity.kt
+++ app/src/main/java/com/xcnk/cryptot/screens/auth/AuthActivity.kt
@@ -1,7 +1,8 @@
-package com.xcnk.cryptot
+package com.xcnk.cryptot.screens.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.xcnk.cryptot.R
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/xcnk/cryptot/screens/auth/AuthFragment.kt
+++ app/src/main/java/com/xcnk/cryptot/screens/auth/AuthFragment.kt
@@ -1,10 +1,11 @@
-package com.xcnk.cryptot
+package com.xcnk.cryptot.screens.auth
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.xcnk.cryptot.R
 
 
 class AuthFragment : Fragment() {
