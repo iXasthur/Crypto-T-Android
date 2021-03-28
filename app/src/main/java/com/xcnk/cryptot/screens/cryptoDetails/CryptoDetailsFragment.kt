16946183b63d82b24447bf238c8f3deba4e1fa2d
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2021-03-29 00:34:53 +0300

    Implemented basic details view

--- app/src/main/java/com/xcnk/cryptot/screens/cryptoDetails/CryptoDetailsFragment.kt
+++ app/src/main/java/com/xcnk/cryptot/screens/cryptoDetails/CryptoDetailsFragment.kt
@@ -2,12 +2,32 @@ package com.xcnk.cryptot.screens.cryptoDetails
 
 import android.os.Bundle
 import android.view.*
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.squareup.picasso.Picasso
 import com.xcnk.cryptot.R
+import com.xcnk.cryptot.api.Session
+import de.hdodenhof.circleimageview.CircleImageView
 
 
 class CryptoDetailsFragment : Fragment() {
+
+    private lateinit var iconView: CircleImageView
+    private lateinit var nameTextView: TextView
+    private lateinit var codeTextView: TextView
+    private lateinit var descriptionTextView: TextView
+    private lateinit var videoView: VideoView
+    private lateinit var videoStackView: LinearLayout
+    private lateinit var eventLatitudeTextView: TextView
+    private lateinit var eventLongitudeTextView: TextView
+    private lateinit var eventNoteTextView: TextView
+    private lateinit var eventStackView: LinearLayout
+
+    override fun onResume() {
+        super.onResume()
+        syncWithSelected()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,15 +45,20 @@ class CryptoDetailsFragment : Fragment() {
     }
 
     private fun initViewObjects(view: View) {
-
+        iconView = view.findViewById(R.id.fragment_crypto_details_icon)
+        nameTextView = view.findViewById(R.id.fragment_crypto_details_text_name)
+        codeTextView = view.findViewById(R.id.fragment_crypto_details_text_code)
+        descriptionTextView = view.findViewById(R.id.fragment_crypto_details_text_description)
+        videoView = view.findViewById(R.id.fragment_crypto_details_video)
+        videoStackView = view.findViewById(R.id.fragment_crypto_details_video_stack)
+        eventLatitudeTextView = view.findViewById(R.id.fragment_crypto_details_text_event_latitude)
+        eventLongitudeTextView = view.findViewById(R.id.fragment_crypto_details_text_event_longitude)
+        eventNoteTextView = view.findViewById(R.id.fragment_crypto_details_text_event_note)
+        eventStackView = view.findViewById(R.id.fragment_crypto_details_event_stack)
     }
 
     private fun setupViewObjects() {
-        setupButtonListeners()
-    }
-
-    private fun setupButtonListeners() {
-
+        videoView.setMediaController(MediaController(requireContext()));
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -52,6 +77,42 @@ class CryptoDetailsFragment : Fragment() {
                 true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun syncWithSelected() {
+        val asset = Session.selectedAsset
+        if (asset != null) {
+            if (asset.iconFileData != null) {
+                Picasso.get()
+                    .load(asset.iconFileData!!.downloadURL)
+                    .placeholder(R.drawable.ic_cryptos)
+                    .error(R.drawable.ic_cryptos)
+                    .into(iconView)
+            } else {
+                iconView.setImageResource(R.drawable.ic_cryptos)
+            }
+
+            nameTextView.text = asset.name
+            codeTextView.text = asset.code
+            descriptionTextView.text = asset.description
+
+            if (asset.videoFileData != null) {
+                videoView.setVideoPath(asset.videoFileData!!.downloadURL)
+                videoView.seekTo(100)
+                videoStackView.visibility = View.VISIBLE
+            } else {
+                videoStackView.visibility = View.GONE
+            }
+
+            if (asset.suggestedEvent != null) {
+                eventLatitudeTextView.text = asset.suggestedEvent!!.latitude.toString()
+                eventLongitudeTextView.text = asset.suggestedEvent!!.longitude.toString()
+                eventNoteTextView.text = asset.suggestedEvent!!.note
+                eventStackView.visibility = View.VISIBLE
+            } else {
+                eventStackView.visibility = View.GONE
+            }
         }
     }
 }
