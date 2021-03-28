b785a144dcfc66c5236e311b08aead180f2eb0e3
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2021-03-28 13:59:01 +0300

    Update AuthFragment.kt

--- app/src/main/java/com/xcnk/cryptot/screens/auth/AuthFragment.kt
+++ app/src/main/java/com/xcnk/cryptot/screens/auth/AuthFragment.kt
@@ -5,17 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.xcnk.cryptot.R
 
 
 class AuthFragment : Fragment() {
 
+    private lateinit var signUpButton: Button
+    private lateinit var signInButton: Button
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_auth, container, false)
+        val view = inflater.inflate(R.layout.fragment_auth, container, false)
+        initViewObjects(view)
+        setupViewObjects()
+        return view
+    }
+
+    private fun initViewObjects(view: View) {
+        signInButton = view.findViewById(R.id.fragment_auth_button_sign_in)
+        signUpButton = view.findViewById(R.id.fragment_auth_button_sign_up)
+    }
+
+    private fun setupViewObjects() {
+        setupButtonListeners()
+    }
+
+    private fun setupButtonListeners() {
+        signInButton.setOnClickListener {
+
+        }
+
+        signUpButton.setOnClickListener {
+
+        }
     }
 
 }
